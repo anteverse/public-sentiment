9db49842c07e9dce1ac17a518f56da450d6c5a40
anteverse <aurelien.didier@gmail.com>
2016-04-18 01:48:18 -0700

    updating test_app

--- test_app.sh
+++ test_app.sh
@@ -5,3 +5,7 @@ PORT=5000                       #
 
 # coordinates are random
 curl -XGET "http://$HOST:$PORT/location/-90.262,38.640,3"
+curl -XDELETE "http://$HOST:$PORT/location/-90.262,38.640,3"
+curl -XGET "http://$HOST:$PORT/location/"
+curl -XPOST "http://$HOST:$PORT/location/" -d '{ "data": "data" }'
+curl -XPUT "http://$HOST:$PORT/location/" -d '{ "data": "data" }'
